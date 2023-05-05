113d8287a70a5266701c757bb93b457bf4bab2b7
getoffdeez <111947685+getoffdeez@users.noreply.github.com>
2023-05-04 21:00:03 -0800

    added some player inputs

--- Assets/Leapold.cs
+++ Assets/Leapold.cs
@@ -11,16 +11,35 @@ public class Leapold : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("If you press the up arrow you'll JUMP");
-        Debug.Log("If you Press the right arrow " + numberOfTimes + " you'll move!");
+        //Debug.Log("If you press the up arrow you'll JUMP");
+        //Debug.Log("If you Press the right arrow " + numberOfTimes + " you'll move!");
 
-        Debug.LogWarning("If you press the " + nameOfTheKey + ", nothing happens");
-        Debug.LogError("If you smash the keyboard at a speed of " + speedOfBreaking + " nothing happens, you just cry");
+        //Debug.LogWarning("If you press the " + nameOfTheKey + ", nothing happens");
+        //Debug.LogError("If you smash the keyboard at a speed of " + speedOfBreaking + " nothing happens, you just cry");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up Arrow Key was pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow Key was pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("Left Arrow Key was pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            print("Right Arrow Key was pressed");
+        }
+
     }
 }
