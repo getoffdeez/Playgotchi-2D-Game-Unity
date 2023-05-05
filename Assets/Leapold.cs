4eddd15abdfe83868dbbcbfcb03960c87a947f10
getoffdeez <111947685+getoffdeez@users.noreply.github.com>
2023-05-04 20:22:06 -0800

    added script for debug.log

---
+++ Assets/Leapold.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leapold : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Press the up key to jump");
+        Debug.Log("Press the right key to go forward");
+        Debug.LogWarning("Pressing the space key does nothing");
+        Debug.LogError("If you break your keyboard you will be keyless");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
